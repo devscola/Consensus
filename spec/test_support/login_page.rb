843a994ff90327b1eadb1eb9bedc751a5efdff18
Interdictor <interdictor.dsg@gmail.com>
2017-02-22 11:27:59 +0100

    Extract static_spec to login_page_spec

--- spec/test_support/login_page.rb
+++ spec/test_support/login_page.rb
@@ -5,12 +5,30 @@ module TestSupport
     def initialize
       url = '/index.html'
       visit(url)
+      validate!
     end
 
-    def login(username, password)
+    def sing_in(username, password)
       fill_in('username', with: username)
       fill_in('password', with: password)
       click_button('submit')
+    end
+
+    def dismiss_wrong_credentials_warning
+      click_button('dismiss-error')
+    end
+
+    def toggle_password_visibility
+      find('#passwordToggler').click
+    end
+
+    private
+
+    def validate!
+      page.assert_selector('#username')
+      page.assert_selector('#password')
+      page.assert_selector('#submit')
+      page.assert_selector('#passwordToggler')
     end
   end
 end
