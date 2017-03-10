babfc9568a8bbdb35ceeadc16e587657e9cf6374
elferrer <raul@cuchame.es>
2017-03-10 14:11:04 +0100

    segregate local user and docker user

--- spec/spec_helper_bdd.rb
+++ spec/spec_helper_bdd.rb
@@ -1,16 +1,39 @@
 require 'sinatra'
 require 'capybara'
 require 'capybara/rspec'
+require 'selenium-webdriver'
 
-Sinatra::Application.environment = :test
-Capybara.app = Sinatra::Application
+if (ENV['CONSENSUS_MODE'])
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, {
+      browser: :remote,
+      url: "http://chrome-browser:4444/wd/hub",
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome
+    })
+  end
+  Capybara.default_driver = :chrome
+  
+  ip = %x(/sbin/ip route|awk '/default/ { print $3 }').strip
+  Capybara.app_host = "http://#{ip}:4567"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+
+else
+
+  Sinatra::Application.environment = :test
+
+  Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+
+  Capybara.default_driver = :chrome
+
+  Capybara.app_host = "http://localhost:4567"
+  Capybara.server_host = "localhost"
+  Capybara.server_port = "4567"
 end
 
-Capybara.default_driver = :chrome
 
-Capybara.app_host = "http://localhost:4567"
-Capybara.server_host = "localhost"
-Capybara.server_port = "4567"
+
+  
+
+
