6be5405b4dfa5a186a1756e6f71bb903d87ba5e3
Manel Romero <manel@manelromero.com>
2017-03-13 12:13:36 +0100

    Remove unused code

--- public/js/components/listSubmit.js
+++
@@ -1,9 +0,0 @@
-var ListSubmit = function(){
-    var listSubmit = document.getElementById('list-submit');
-
-    var buttonClicked = function(){
-        Bus.publish('button clicked');
-    };
-
-    listSubmit.addEventListener('click', buttonClicked);  
-};
--- public/js/pages/listPage.js
+++
@@ -1,5 +0,0 @@
-var ListPage = function() {
-    new ListInput();
-    new ListSubmit();
-    new ListUl();
-};
