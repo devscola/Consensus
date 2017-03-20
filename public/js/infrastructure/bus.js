37072dc95520f7eb99ee5d119d6db88dccb6692a
Manel Romero <manel@manelromero.com>
2017-03-20 11:20:22 +0100

    Bus allows no subscriptions to a topic

--- public/js/infrastructure/bus.js
+++ public/js/infrastructure/bus.js
@@ -1,20 +1,22 @@
 var Bus = {
 
-    topics: [],
+    subscriptions: {},
+
     subscribe: function(topic, handler) {
         this.ensureTopic(topic);
         this.subscriptions[topic].push(handler);
     },
-    subscriptions: {},
+
     publish: function(topic, data) {
+        if (!this.subscriptions[topic]) return;
         this.subscriptions[topic].forEach(function(handler) {
             handler(data);
         });
     },
+
     ensureTopic: function(topic) {
         if (this.subscriptions[topic]) return;
 
         this.subscriptions[topic] = [];
-        this.topics.push(topic);
     }
 };
