d4430dfb8ff61a3c4e53d8d2e0d211f8049528ae
Interdictor <interdictor.dsg@gmail.com>
2017-04-24 17:35:00 +0200

    Change target folder of Support::Courier

--- support/courier.rb
+++ support/courier.rb
@@ -9,16 +9,16 @@ module Support
 
       private
 
-      def collect_polymer
+      def collect_polymer_files
         polymer_main = open('https://polygit.org/components/polymer/polymer.html')
         polymer_mini = open('https://polygit.org/components/polymer/polymer-mini.html')
         polymer_micro = open('https://polygit.org/components/polymer/polymer-micro.html')
         webcomponents_js = open('https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.24/webcomponents-lite.min.js')
 
-        @engine_main = File.open('vendor/polymer/polymer.html', 'w')
-        @engine_mini = File.open('vendor/polymer/polymer-mini.html', 'w')
-        @engine_micro = File.open('vendor/polymer/polymer-micro.html', 'w')
-        @engine_webcomponents = File.open('vendor/polymer/webcomponents-lite.min.js', 'w')
+        @engine_main = File.open('public/vendor/polymer/polymer.html', 'w')
+        @engine_mini = File.open('public/vendor/polymer/polymer-mini.html', 'w')
+        @engine_micro = File.open('public/vendor/polymer/polymer-micro.html', 'w')
+        @engine_webcomponents = File.open('public/vendor/polymer/webcomponents-lite.min.js', 'w')
 
         @engine_main.puts(polymer_main.readlines)
         @engine_mini.puts(polymer_mini.readlines)
