6524dbd75d5f9aa12e9a7e098df7c1ced184d3d7
Interdictor <interdictor.dsg@gmail.com>
2017-04-27 14:16:07 +0200

    Remove unused code in Support::Courier

--- support/courier.rb
+++ support/courier.rb
@@ -17,20 +17,20 @@ module Support
         polymer_micro = open('https://polygit.org/components/polymer/polymer-micro.html')
         webcomponents_js = open('https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/0.7.24/webcomponents-lite.min.js')
 
-        @engine_main = File.open('public/vendor/polymer/polymer.html', 'w')
-        @engine_mini = File.open('public/vendor/polymer/polymer-mini.html', 'w')
-        @engine_micro = File.open('public/vendor/polymer/polymer-micro.html', 'w')
-        @engine_webcomponents = File.open('public/vendor/polymer/webcomponents-lite.min.js', 'w')
+        engine_main = File.open('public/vendor/polymer/polymer.html', 'w')
+        engine_mini = File.open('public/vendor/polymer/polymer-mini.html', 'w')
+        engine_micro = File.open('public/vendor/polymer/polymer-micro.html', 'w')
+        engine_webcomponents = File.open('public/vendor/polymer/webcomponents-lite.min.js', 'w')
 
-        @engine_main.puts(polymer_main.readlines)
-        @engine_mini.puts(polymer_mini.readlines)
-        @engine_micro.puts(polymer_micro.readlines)
-        @engine_webcomponents.puts(webcomponents_js.readlines)
+        engine_main.puts(polymer_main.readlines)
+        engine_mini.puts(polymer_mini.readlines)
+        engine_micro.puts(polymer_micro.readlines)
+        engine_webcomponents.puts(webcomponents_js.readlines)
 
-        @engine_main.close
-        @engine_mini.close
-        @engine_micro.close
-        @engine_webcomponents.close
+        engine_main.close
+        engine_mini.close
+        engine_micro.close
+        engine_webcomponents.close
 
         puts 'Support::Courier: Polymer files collected.'
       end
