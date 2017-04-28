827dad9e3d6962f508b329c2c0ab5071f0d1a12f
Interdictor <interdictor.dsg@gmail.com>
2017-04-28 17:23:49 +0200

    Extract method connection

--- services/proposals/repository.rb
+++ services/proposals/repository.rb
@@ -4,7 +4,6 @@ Mongo::Logger.logger.level = ::Logger::INFO
 
 module Proposals
   class Repository
-   
     class << self
       def store(title = '', content, proposer)
         id = generate_id(title.to_s, content.to_s)
@@ -15,39 +14,24 @@ module Proposals
       end
 
       def save(proposal)
-        client = Mongo::Client.new([ 'mongocontainer:27017' ],
-         :database => 'consensus_db')
-        collection = client[:proposals]
         collection.insert_one(proposal.to_h)
       end
 
       def update(proposal)
-        client = Mongo::Client.new([ 'mongocontainer:27017' ],
-         :database => 'consensus_db')
-        collection = client[:proposals]
         document=proposal.to_h
         collection.find_one_and_replace({id: document[:id]},document)
       end
 
       def retrieve(id)
-        client = Mongo::Client.new([ 'mongocontainer:27017' ],
-        :database => 'consensus_db')
-        collection = client[:proposals]
         data=collection.find({id: id}).first
         Proposal.from_bson(data)
       end
 
       def all
-        client = Mongo::Client.new([ 'mongocontainer:27017' ],
-         :database => 'consensus_db')
-        collection = client[:proposals]
         collection.find()
       end
 
       def empty
-        client = Mongo::Client.new([ 'mongocontainer:27017' ],
-         :database => 'consensus_db')
-        collection = client[:proposals]
         collection.delete_many()
       end
 
@@ -55,6 +39,15 @@ module Proposals
 
       def generate_id(*identifiers)
         Digest::MD5.hexdigest(identifiers.join)
+      end
+
+      def connection
+        @connection ||= Mongo::Client.new([ 'mongocontainer:27017' ],
+                 :database => 'consensus_db')
+      end
+
+      def collection
+        connection[:proposals]
       end
 
       class Proposal
@@ -89,7 +82,7 @@ module Proposals
         def involved? person
           @circle.include?(person)
         end
-        
+
         def create_circle(a_circle)
           a_circle.each{|person| involve(person)}
         end
