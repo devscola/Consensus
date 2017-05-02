4d53b51ffc089174fabf4419bb0c097ca04aa2e9
Interdictor <interdictor.dsg@gmail.com>
2017-05-02 14:58:30 +0200

    Remove unnecesary heritage in Pages.Login.js

--- public/js/pages/login.js
+++ public/js/pages/login.js
@@ -1,25 +1,22 @@
 Class('Page.Login', {
 
-    Extends: Page,
-
     STATIC: {
         HOME: '/'
     },
 
     initialize: function() {
-        instances = [Login.Form, Services.Login];
-        Page.Login.Super.call(this, instances);
+        new Login.Form();
+
+        new Services.Login();
         new Services.Storage();
+        this.subscribe();
     },
 
     goToHome: function() {
         window.location = Page.Login.HOME;
     },
 
-    publish: function() {},
-
     subscribe: function() {
         Bus.subscribe('attemp.succeeded', this.goToHome);
     }
-
 });
