8a818d112ee3fa0583baefeea2db8493928edf33
Manel Romero <manel@manelromero.com>
2017-05-05 12:39:42 +0200

    Remove unused code

--- public/js/services/userList.js
+++ public/js/services/userList.js
@@ -18,16 +18,9 @@ Class('Services.UserList', {
         });
     },
 
-    retrieve: function(id) {
-        this.doRequest('/users/retrieve', id, function(result) {
-            Bus.publish('proposal.circle.retrieved', result.circle);
-        });
-    },
-
     subscribe: function() {
         Bus.subscribe('users.retrieve', this.list.bind(this));
         Bus.subscribe('proposal.user.add', this.add.bind(this));
-        Bus.subscribe('proposal.circle.retrieve', this.retrieve.bind(this));
     }
 
 });
