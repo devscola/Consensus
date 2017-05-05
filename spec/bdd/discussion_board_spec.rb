da924a1c9f32377acc11dfabc96199097c53f89b
Interdictor <interdictor.dsg@gmail.com>
2017-05-05 13:30:55 +0200

    Improve semantics in discussion_board_spec.rb

--- spec/bdd/discussion_board_spec.rb
+++ spec/bdd/discussion_board_spec.rb
@@ -10,57 +10,57 @@ feature 'Discussion board' do
     Fixture.proposal_with_a_user_involved
   end
 
-  scenario 'Shows circle users including the proposer' do
+  scenario 'shows involved including the proposer' do
     board = Fixture.proposal_visited
     expect(board.circle).to include(Fixture::PROPOSER, Fixture::INVOLVED)
   end
 
-  scenario 'Show question button if user in circle' do
+  scenario 'allows questioning when user involved' do
     Fixture.involved_logged
     board = Fixture.proposal_visited
     expect(board.question_button?).to be true
   end
 
-  scenario "Doesn't show question button if user not in circle" do
+  scenario 'disallows questioning when user not involved' do
     Fixture.not_involved_logged
     board = Fixture.proposal_visited
     expect(board.question_button?).to be false
   end
 
-  scenario 'Submit question activates when enough text' do
+  scenario 'enables submit with enough text' do
     board = Fixture.involved_is_questioning
     board.fill_enough_text
     expect(board.submit_question_active?).to be true
   end
 
-  scenario 'Submit question doesnt activate without enough text' do
+  scenario 'disables submit without enough text' do
     board = Fixture.involved_is_questioning
     board.fill_not_enough_text
     expect(board.submit_question_active?).to be false
   end
 
-  scenario 'Shows textarea when question button is clicked' do
+  scenario 'allows question writing' do
     board = Fixture.involved_is_questioning
     board.create_question
     expect(board.question_button_active?).to be false
     expect(board.question_content?).to be true
   end
 
-  scenario 'Does not show question button for proposal user' do
+  scenario 'disallows question writing after question submit' do
+    board = Fixture.involved_is_questioning
+    board.submit_question_form
+    expect(board.question_form?).to be false
+  end
+
+  scenario 'disallows proposer to question' do
     Fixture.login
     board = Fixture.proposal_visited
     expect(board.question_button_visible?).to be false
   end
 
-  scenario 'Question button enabled when question is submited' do
+  scenario 'allows questioning after question submit' do
     board = Fixture.involved_is_questioning
     board.submit_question_form
     expect(board.question_button_active?).to be true
-  end
-
-  scenario 'Question form disabled when question is submited' do
-    board = Fixture.involved_is_questioning
-    board.submit_question_form
-    expect(board.question_form?).to be false
   end
 end
